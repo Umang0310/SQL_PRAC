8de30da2da34791d7a2d078d6181fda447d4851a
Umang0310 <prajapatiumang2004@gmail.com>
2025-11-29 00:08:56 +0530

    Basic Select Statement with concat

---
+++ 01B_SelectStatement.sql
@@ -0,0 +1,16 @@
+SELECT * FROM Employees
+
+SELECT FirstName FROM Employees
+
+SELECT FirstName,Salary FROM Employees
+
+SELECT FirstName,LastName FROM Employees
+
+SELECT EmployeeID,CONCAT(firstname,' ',lastname) FROM Employees --used to merge two cloumns
+
+SELECT EmployeeID,CONCAT(LastName,' ',FirstName) FROM Employees --example of concat
+
+SELECT EmployeeID,CONCAT(firstname,' ',lastname) [FULL NAME] FROM Employees --ADD COLUMN FOR THE CONCATINATION
+
+SELECT EmployeeID,CONCAT(firstname,' ',lastname) AS [FULL NAME] FROM Employees --ADD ALIASES FOR THE CONCAT
+
