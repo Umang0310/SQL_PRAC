d523f3100d02db1459352ae6a05b25681bb36ec5
Umang0310 <prajapatiumang2004@gmail.com>
2025-11-30 23:46:01 +0530

    SQL Select Distinct Statement

--- 02B_SelectDistinct.sql
+++ 02B_SelectDistinct.sql
@@ -12,7 +12,7 @@ select distinct * from EmployeeRecords
 --whole table appears because there is no such case
 --where there are no completely duplicate records.
   
- 
+  
 --The SQL DISTINCT statement is used to retrieve unique values from a column or combination of columns in a table.
 --You can use SELECT DISTINCT with one or more columns to get unique combinations of those columns.
 --SELECT DISTINCT can also be used to retrieve unique records from an entire table.
