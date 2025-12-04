60e7f78590836aa4748fcebb8b5068796cd56e5a
Umang0310 <prajapatiumang2004@gmail.com>
2025-12-04 22:37:46 +0530

    SQL AND & OR Operator

--- 06_And-Or-Operator.sql
+++ 06_And-Or-Operator.sql
@@ -9,7 +9,7 @@ where Department = 'HR' or Department = 'Finance'
 
 select * from EmployeeRecords
 where (Department = 'HR' or Department = 'Finance') and Salary = 60000
-
+ 
 --The AND operator in SQL is used to filter records when all specified conditions must be met.
 --The OR operator is used when at least one of the specified conditions must be true for a record to be included in the results.
 --Parentheses can be used to group conditions and control the logical evaluation order when combining AND and OR operators.
