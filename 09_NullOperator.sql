eccc8264ce1ba08709a0fdc7ad18dea44318ad06
Umang0310 <prajapatiumang2004@gmail.com>
2025-12-05 22:41:25 +0530

    SQL NULL Operator

--- 09_NullOperator.sql
+++ 09_NullOperator.sql
@@ -9,7 +9,7 @@ insert into dbo.Employees
 values (10,'Harsh','','0',50300,'2021-10-11')
 
 select * from dbo.Employees where Department = null
-
+ 
 --Executing this above statement does not return any results, nor does it throw an error. 
 --This is because the correct way to check for null values is to use the IS NULL operator.
 
