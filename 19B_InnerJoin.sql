194bcb2c5c0cc49e1df9ce99c77ef327e8a17a08
Umang0310 <prajapatiumang2004@gmail.com>
2025-12-10 22:20:42 +0530

    SQL Inner Join

--- 19B_InnerJoin.sql
+++ 19B_InnerJoin.sql
@@ -7,7 +7,7 @@ on table3.c1=table4.c1
 
 select table3.c1, table3.c2, table4.c3 from table3
 inner join table4 on table3.c1=table4.c1
-
+ 
 select a.c1,a.c2,b.c3 from table3 a
 inner join table4 b
 on a.c1=b.c1
