00c8be33949a2e5e29c2989009a50a9f39fc2416
Umang0310 <prajapatiumang2004@gmail.com>
2025-12-10 22:21:21 +0530

    SQL Inner Join

--- 19B_InnerJoin.sql
+++ 19B_InnerJoin.sql
@@ -15,7 +15,7 @@ on a.c1=b.c1
 select a.c1,a.c2,b.c3 from table3 a
 join table4 b
 on a.c1=b.c1
-
+ 
 --Inner Join combines rows from two tables based on a common column, returning only matching rows.
 --Null values in the join column are ignored during the join operation.
 --The SELECT statement can be customized to display only specific columns from the joined tables.
