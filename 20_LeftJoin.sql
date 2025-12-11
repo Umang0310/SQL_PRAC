bd1db7a4d919f4f9ca3e014585fd83546989310c
Umang0310 <prajapatiumang2004@gmail.com>
2025-12-11 23:12:47 +0530

    SQL Left Join

--- 20_LeftJoin.sql
+++ 20_LeftJoin.sql
@@ -12,7 +12,7 @@ on a.c1 = b.c1
 select a.c1,a.c2,b.c3 from table1 a 
 left join table2 b 
 on a.c1=b.c1
-
+ 
 --The Left Outer Join returns all records from the left table and matched records from the right table.
 --If there is no match in the right table, the result contains NULLs for columns from the right table.
 --The join condition is specified using the ON clause, comparing columns from both tables.
