a8abb282ae5be365e708630ad1bee64a3d1f24fe
Umang0310 <prajapatiumang2004@gmail.com>
2025-12-11 23:13:02 +0530

    SQL Right Join

--- 21_RightJoin.sql
+++ 21_RightJoin.sql
@@ -10,7 +10,7 @@ on table3.c1 = table4.c1
 
 select b.c1,a.c2,b.c3 from table3 a right join
 table4 b on a.c1=b.c1
-
+ 
 --Right join (or right outer join) returns all records from the right table and the matched records from the left table.
 --If there is no match, NULL values are returned for columns from the left table.
 --The join predicate is based on matching column values between the two tables.
