a511dabc29d6f34fa18261a22d7a054e1c068d14
Umang0310 <prajapatiumang2004@gmail.com>
2025-12-17 22:13:51 +0530

    SQL Sub Queries

--- 34B_SubQueries.sql
+++ 34B_SubQueries.sql
@@ -6,7 +6,7 @@ select * from customers
 where customer_id in
 (select distinct customer_id from orders where order_date between '2024-08-01' and '2024-08-31')
  
-select * from customers
+select * from customers 
 where customer_id in
 (select distinct customer_id from orders where order_date not between '2024-08-01' and '2024-08-31')
 
