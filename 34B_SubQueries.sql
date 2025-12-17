44174fed919330957c78e4580edb4c118aab2036
Umang0310 <prajapatiumang2004@gmail.com>
2025-12-17 22:13:15 +0530

    SQL Sub Queries

--- 34B_SubQueries.sql
+++ 34B_SubQueries.sql
@@ -1,7 +1,7 @@
 select * from Customers
 
 select * from Orders
- 
+  
 select * from customers
 where customer_id in
 (select distinct customer_id from orders where order_date between '2024-08-01' and '2024-08-31')
