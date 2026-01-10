64d239bc4fbdb46ee8a096014691f48a3c5e1bab
Umang0310 <prajapatiumang2004@gmail.com>
2026-01-10 21:33:55 +0530

    Clustered Index in MS SQL Server

--- 55_ClusteredIndex.sql
+++ 55_ClusteredIndex.sql
@@ -7,7 +7,7 @@ drop table if exists students
 create table Students ( id int, name nvarchar(50), age int, gender varchar(20))
  
 insert into Students values
-(1,'Raj',23,'M'),
+(1,'Raj',23,'M'), 
 (4,'Nitin',25,'M'),
 (3,'Priya',21,'F'),
 (2,'Nikita',20,'F'),
